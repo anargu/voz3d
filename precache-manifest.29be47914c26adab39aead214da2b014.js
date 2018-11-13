f95552a6d381183751528b52fd6392fa249be801
anthony.arostegui <anthony.arostegui@bbva.com>
2018-11-12 19:26:50 -0500

    adding dist folder

---
+++ precache-manifest.29be47914c26adab39aead214da2b014.js
@@ -0,0 +1,350 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a9305a6d3ccdd3c6d6d2fd44ed2b576",
+    "url": "assets/manifest/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "384faf404912052fd3251f077eeb556f",
+    "url": "index.html"
+  },
+  {
+    "revision": "449e154f5a93678178caf6d1d5857d65",
+    "url": "vendor/bundles/webcomponents-sd-ce-pf.js"
+  },
+  {
+    "revision": "893102e1869adc5bce0a94feb6f74249",
+    "url": "vendor/webcomponents-bundle.js"
+  },
+  {
+    "revision": "abb358dc9afad389c31bee5903095e06",
+    "url": "vendor/bundles/webcomponents-sd-ce.js"
+  },
+  {
+    "revision": "da48dd1cd76dee364cc403d607193f16",
+    "url": "vendor/bundles/webcomponents-sd.js"
+  },
+  {
+    "revision": "481edf10befe297681b42b8ce8041f50",
+    "url": "vendor/web-animations-next.min.js"
+  },
+  {
+    "revision": "0f9a5cf89f1d4757a6a4945a329e1fc0",
+    "url": "vendor/bundles/webcomponents-ce.js"
+  },
+  {
+    "revision": "d42dd0732b85d5efa9e1660a1747b05c",
+    "url": "vendor/webcomponents-loader.js"
+  },
+  {
+    "revision": "8391957aaff84ed2243736878b5b1108",
+    "url": "vendor/custom-elements-es5-adapter.js"
+  },
+  {
+    "revision": "3f67e3ce12b883499b68727325605d46",
+    "url": "assets/polos.jpg"
+  },
+  {
+    "revision": "14e485c4615b4d5c1458",
+    "url": "bundled.js"
+  },
+  {
+    "revision": "74d886e675ec2a23d9bd29fbaf3674c0",
+    "url": "assets/ojo.jpg"
+  },
+  {
+    "revision": "b2818b25420ed960e98d223536901151",
+    "url": "assets/twitter.svg"
+  },
+  {
+    "revision": "3ebcb8c6551edd234ecdcee8fdd04ada",
+    "url": "assets/words.json"
+  },
+  {
+    "revision": "e92cc0fb9e1a7debc138224fd02a462a",
+    "url": "assets/roboto-mono-v5-latin-regular.woff2"
+  },
+  {
+    "revision": "d4b9b85b9c478cd3fe6ac635ded6b557",
+    "url": "assets/share.svg"
+  },
+  {
+    "revision": "3594188da8a71e7a98cb05b735f3b384",
+    "url": "assets/smile.svg"
+  },
+  {
+    "revision": "062bcb74395f0ff1db78c5c8af1bceff",
+    "url": "assets/facebook.svg"
+  },
+  {
+    "revision": "e4957528f9ceb54959f34e01e5e23931",
+    "url": "assets/question.svg"
+  },
+  {
+    "revision": "e016c30cb4af6983aa394e8079cf6ae5",
+    "url": "assets/search.svg"
+  },
+  {
+    "revision": "9f6b2b0c9ea7cdb308147a773638fe17",
+    "url": "assets/elipsis-h.svg"
+  },
+  {
+    "revision": "4cd470452db30670aadfe70e4cc8c189",
+    "url": "assets/manifest/apple-icon-57x57.png"
+  },
+  {
+    "revision": "369b69bf0e7bcff27ea6868da55c9384",
+    "url": "assets/manifest/apple-icon-72x72.png"
+  },
+  {
+    "revision": "cd7cb331942e3b21cf775da59497f218",
+    "url": "assets/manifest/apple-icon-76x76.png"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "assets/manifest/browserconfig.xml"
+  },
+  {
+    "revision": "cb434a3d630b9b1df46052cdf802a50d",
+    "url": "assets/manifest/favicon-16x16.png"
+  },
+  {
+    "revision": "25d77d450d452a3ed08d15325a596e8f",
+    "url": "assets/manifest/favicon-32x32.png"
+  },
+  {
+    "revision": "d0d1d5bf8bbeca412eb55636eac48217",
+    "url": "assets/manifest/favicon-96x96.png"
+  },
+  {
+    "revision": "0ccd5a9b23fb2e878c25e788f6055e1b",
+    "url": "assets/manifest/favicon.ico"
+  },
+  {
+    "revision": "b859a978a161181e5f14ec2d5f12c8e9",
+    "url": "assets/manifest/manifest.json"
+  },
+  {
+    "revision": "0201a0de60af2864575e9de82560089c",
+    "url": "assets/manifest/ms-icon-70x70.png"
+  },
+  {
+    "revision": "502a752b42e1c28a30a58a9a8cd086c7",
+    "url": "assets/manifest/apple-icon-60x60.png"
+  },
+  {
+    "revision": "06be0bf0e781e9f9992de7af0d60b2df",
+    "url": "assets/play.svg"
+  },
+  {
+    "revision": "f1b78bff268d737f32c25dc988fc9343",
+    "url": "assets/manifest/ms-icon-310x310.png"
+  },
+  {
+    "revision": "6a9305a6d3ccdd3c6d6d2fd44ed2b576",
+    "url": "assets/manifest/apple-icon.png"
+  },
+  {
+    "revision": "d352128bbb6f8f7705e6ce25276fcbfc",
+    "url": "assets/mic.svg"
+  },
+  {
+    "revision": "500ef86ddb32f387f650acac1b59d3c0",
+    "url": "assets/manifest/ms-icon-144x144.png"
+  },
+  {
+    "revision": "3f9a35faa7c429bdc2da442a0ba89b07",
+    "url": "assets/manifest/ms-icon-150x150.png"
+  },
+  {
+    "revision": "20621ea3e497dfcf9bcd3b50b7e57052",
+    "url": "assets/head/3_ENOJADO.json"
+  },
+  {
+    "revision": "c5207efc235f6209c33b4b5c3744485d",
+    "url": "assets/manifest/apple-icon-152x152.png"
+  },
+  {
+    "revision": "cfc99afda591cf136bb6630be6723cee",
+    "url": "assets/manifest/apple-icon-180x180.png"
+  },
+  {
+    "revision": "e92ecdf1f1d834897b7b4eda578981ff",
+    "url": "assets/body/atencion_005.json"
+  },
+  {
+    "revision": "bd56a81261475527367a710e937af7cb",
+    "url": "assets/manifest/android-icon-48x48.png"
+  },
+  {
+    "revision": "500ef86ddb32f387f650acac1b59d3c0",
+    "url": "assets/manifest/apple-icon-144x144.png"
+  },
+  {
+    "revision": "a0e54bb2923016cd3e565d1521e46f9f",
+    "url": "assets/manifest/apple-icon-120x120.png"
+  },
+  {
+    "revision": "369b69bf0e7bcff27ea6868da55c9384",
+    "url": "assets/manifest/android-icon-72x72.png"
+  },
+  {
+    "revision": "39ce6f4eb14601dfa34d3070942f8ccd",
+    "url": "assets/manifest/apple-icon-114x114.png"
+  },
+  {
+    "revision": "d0d1d5bf8bbeca412eb55636eac48217",
+    "url": "assets/manifest/android-icon-96x96.png"
+  },
+  {
+    "revision": "204369638561d9350969c094243e859d",
+    "url": "assets/body/himnonacional_0034.json"
+  },
+  {
+    "revision": "e58e9c607d7bce695eed98cb3e5c8df5",
+    "url": "assets/manifest/android-icon-36x36.png"
+  },
+  {
+    "revision": "cac6fa9f5aaeca84d13bb06fccb208cb",
+    "url": "assets/body/acercarme_0001.json"
+  },
+  {
+    "revision": "b3565a860fdb6b93f4eaa6984225dcdc",
+    "url": "assets/manifest/android-icon-192x192.png"
+  },
+  {
+    "revision": "bf89b056a4de4b85d5b49288961bb1de",
+    "url": "assets/body/equivocaste_0025.json"
+  },
+  {
+    "revision": "1a4150d2c884704e6fc96e9dba084c9d",
+    "url": "assets/cuerpo.jpg"
+  },
+  {
+    "revision": "f5f9ae5275e662ed978b6240282f2014",
+    "url": "assets/head.json"
+  },
+  {
+    "revision": "500ef86ddb32f387f650acac1b59d3c0",
+    "url": "assets/manifest/android-icon-144x144.png"
+  },
+  {
+    "revision": "5fdc49bfe23a6b88dfcb8a5e5ceb30df",
+    "url": "assets/mail.svg"
+  },
+  {
+    "revision": "3ed64c4c39f51f040612267597748b3b",
+    "url": "assets/github-alt.svg"
+  },
+  {
+    "revision": "3c7d0b260535dbc0a75d6576f129e336",
+    "url": "assets/head/5_TRISTE.json"
+  },
+  {
+    "revision": "1c37318fbe7aca76f8b1fa94c63bb453",
+    "url": "assets/head/1_DUDOSO.json"
+  },
+  {
+    "revision": "ffbb7491788e6fa409a6b13af39a4652",
+    "url": "assets/head/0_MIEDOSO.json"
+  },
+  {
+    "revision": "e88032df6ab34986df50f50e96165f02",
+    "url": "assets/head/4_FELIZ.json"
+  },
+  {
+    "revision": "f8896e095b7772e9da69a749cb3a7676",
+    "url": "assets/logo.png"
+  },
+  {
+    "revision": "77f00e9f2244520079969d7c162dea36",
+    "url": "assets/body/siento_0079.json"
+  },
+  {
+    "revision": "55348a9752174ae1ca9604584aa83570",
+    "url": "assets/cabellos.jpg"
+  },
+  {
+    "revision": "27983a680ddaac769a5eef3e036cb86f",
+    "url": "assets/coffee.svg"
+  },
+  {
+    "revision": "7d5e13ed59c1e1286fec91ca1fbe3202",
+    "url": "assets/body/celebramos_0012.json"
+  },
+  {
+    "revision": "50517491030ed47a64328ef68fdd461f",
+    "url": "assets/body/cuadernoabran_15.json"
+  },
+  {
+    "revision": "284ed391dd89f1727436874f6f3dbe0b",
+    "url": "assets/body/dia_0020.json"
+  },
+  {
+    "revision": "b6f85e17e398f83f2a79a9fab31f1ab4",
+    "url": "assets/body/que1_0068.json"
+  },
+  {
+    "revision": "0832f95f3a7d74498579f8f82adc5c5b",
+    "url": "assets/body/muybien_0049.json"
+  },
+  {
+    "revision": "0288ab001d5c1078b86102758efb62cc",
+    "url": "assets/body/yo_0090.json"
+  },
+  {
+    "revision": "9b2015ae590b373b0db1be2cd029b8b7",
+    "url": "assets/body/orgulloso1_0056.json"
+  },
+  {
+    "revision": "07553a614fecdeff6440d1241bc5d391",
+    "url": "assets/body/porfavor_0066.json"
+  },
+  {
+    "revision": "8320d7a9c6b99d65e690a9bb6e7d627c",
+    "url": "assets/body/secayo_77.json"
+  },
+  {
+    "revision": "a843abddcf4f48dcaef75faadc285b34",
+    "url": "assets/body/respeta_0073.json"
+  },
+  {
+    "revision": "20a616f5f8e8a8e4d1174dd88cab236f",
+    "url": "assets/body/todos_0082.json"
+  },
+  {
+    "revision": "98e97c455dfd3558b51ef767d8544834",
+    "url": "assets/body/no_0052.json"
+  },
+  {
+    "revision": "6ca18f6d806a038d5f8ec07615e42a7b",
+    "url": "assets/body/voy_0089.json"
+  },
+  {
+    "revision": "65126d9312cfab5f7cf1a6ddf1a7faf6",
+    "url": "assets/body/tequiero_0081.json"
+  },
+  {
+    "revision": "a16fd9292c331460083e2c15a71da59a",
+    "url": "assets/body/escucharmusica_0027.json"
+  },
+  {
+    "revision": "d4fc632ac222b72cb51fd651d08b9ba5",
+    "url": "assets/body/buenosdias1_0010.json"
+  },
+  {
+    "revision": "f9b2abc822545b6adc9a8f1176121156",
+    "url": "assets/body/bien_0007.json"
+  },
+  {
+    "revision": "48c5957fbe9119e8c3904121616363bb",
+    "url": "assets/body/amigos_0003.json"
+  },
+  {
+    "revision": "3df2c85770b6f6cbc5bca0f4252fc587",
+    "url": "assets/body.json"
+  },
+  {
+    "revision": "532deda3675be1feb7b4",
+    "url": "1.bundled.js"
+  }
+];
